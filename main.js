30c267ebf7b8b1be62a1c2f5b4fa2ce4e85764f8
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-09 17:30:05 +0200

    try to fix function

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-//FASE 1: preparazione
-let kilometres = document.getElementById('kilometres');
-
-let age = document.getElementById('age');
 const button = document.getElementById('button');
 
-button.addEventListener('click', ()=>{
-    let kilometresNumber = kilometres.value;
 
-    let ageYears = age.value;
+button.addEventListener('click', ()=>{
+    let kilometres = document.getElementById('kilometres');
+
+    let age = document.getElementById('age');
 
     let ticket_price;
 
     let price = 0.21;
 
-    let young_discount = (price * kilometresNumber *20) / 100;
+    let young_discount = (price * kilometres *20) / 100;
 
-    let senior_discount = (price * kilometresNumber *40) / 100;
-
-    if (ageYears < 18) {
+    let senior_discount = (price * kilometres *40) / 100;
+    
+    if (age < 18) {
     ticket_price = kilometres * price - young_discount;
 
-    }else if (ageYears > 65) {
+    }else if (age > 65) {
     ticket_price = kilometres * price - senior_discount;
 
     }else {
     ticket_price = kilometres * price;
     }
 
-    return(ticket_price.toFixed(2));
+    return ticket_price;
+    
 
 })
+
+console.log(button);
+
