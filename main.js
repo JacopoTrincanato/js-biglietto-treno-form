e65e88e7ee53a53d33e8bde46d40575b80b96970
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-09 17:40:01 +0200

    commit

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 const button = document.getElementById('button');
+let kilometres = document.getElementById('kilometres');
+
+let age = document.getElementById('age');
 
 
 button.addEventListener('click', ()=>{
-    let kilometres = document.getElementById('kilometres');
+    let kilometresEl = kilometres.value;
 
-    let age = document.getElementById('age');
+    let ageEl = age.value;
 
     let ticket_price;
 
     let price = 0.21;
 
-    let young_discount = (price * kilometres *20) / 100;
+    let young_discount = (price * kilometresEl *20) / 100;
 
-    let senior_discount = (price * kilometres *40) / 100;
+    let senior_discount = (price * kilometresEl *40) / 100;
     
-    if (age < 18) {
+    if (ageEl < 18) {
     ticket_price = kilometres * price - young_discount;
 
-    }else if (age > 65) {
+    }else if (ageEl > 65) {
     ticket_price = kilometres * price - senior_discount;
 
     }else {
     ticket_price = kilometres * price;
     }
 
+    console.log(ticket_price.toFixed(2));
+
     return ticket_price;
     
 
 })
-
-console.log(button);
 
