1f4ee15360758e91c6b1607444e8d1224dac92b6
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-09 18:17:46 +0200

    modify form and add some js for milestone 2

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 //MILESTONE 1
 
+/*const button = document.getElementById('button');
+let kilometres = document.getElementById('kilometres');
+
+let age = document.getElementById('age');
+
+
+button.addEventListener('click', ()=>{
+    let kilometresEl = kilometres.value;
+
+    let ageEl = age.value;
+
+    let ticket_price;
+
+    let price = 0.21;
+
+    let young_discount = (price * kilometresEl *20) / 100;
+
+    let senior_discount = (price * kilometresEl *40) / 100;
+    
+    if (ageEl < 18) {
+    ticket_price = kilometresEl * price - young_discount;
+
+    }else if (ageEl > 65) {
+    ticket_price = kilometresEl * price - senior_discount;
+
+    }else {
+    ticket_price = kilometresEl * price;
+    }
+
+    console.log(ticket_price.toFixed(2));
+
+    return ticket_price;
+    
+
+})*/
+
+//MILESTONE 2
+
 const button = document.getElementById('button');
 let kilometres = document.getElementById('kilometres');
 
@@ -35,4 +73,3 @@ button.addEventListener('click', ()=>{
     
 
 })
-
